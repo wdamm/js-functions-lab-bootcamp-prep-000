53ff6858c520f2d3c0f5c00e86ffeb67ef474c08
William Damm <wdamm@powerhrg.com>
2018-12-29 01:42:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ function happyHolidaysTo(name) {
   return(`Happy holidays, ${name}!`)
 }
 
-function happyHolidayTo()
+function happyHolidayTo(holiday,name) {
+  return(`Happy ${holiday}, ${name}!`)
+}
+
