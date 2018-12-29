508ab1c047ebbdf95989ede488828ed9b513b109
William Damm <wdamm@powerhrg.com>
2018-12-29 01:47:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(holiday,name) {
   return(`Happy ${holiday}, ${name}!`)
 }
 
-function holidayCountdown(days, holiday) {
-  return(`It's ${days} days until ${holiday}`)
+function holidayCountdown(days,holiday) {
+  return(`It's ${days} days until ${holiday}!`)
 }
